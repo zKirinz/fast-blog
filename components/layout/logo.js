ab262b2d74adaeff8bec6b29f48154eee8d64186
Trung Kien <kien123456k@gmail.com>
2021-04-23 09:40:43 +0700

    change logo

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import classes from "./logo.module.css";
 
 const Logo = () => {
-  return <div className={classes.logo}>Kien' Fast Blog</div>;
+  return (
+    <div className={classes.logo}>
+      <Image
+        src="/images/logo.png"
+        alt="The Fast Blog logo"
+        width={136}
+        height={76}
+      />
+    </div>
+  );
 };
 
 export default Logo;
